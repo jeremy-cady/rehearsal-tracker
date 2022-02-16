09e5c7734a34e03690e5a6e503e72436ddbde2df
jeremy-cady <jeremy.cady@outlook.com>
2022-02-15 23:20:15 -0600

    production page styled - ProductionDetails Page almost styled

--- src/components/ProductionDetails/ProductionDetails.jsx
+++ src/components/ProductionDetails/ProductionDetails.jsx
@@ -2,6 +2,16 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import moment from 'moment';
+import './ProductionDetails.css';
+
+import { 
+    Table, 
+    TableHead, 
+    TableRow, 
+    TableBody,
+    Button, 
+    Box } 
+from '@mui/material';
 
 function ProductionDetails() {
     console.log('in ProductionDetails');
@@ -74,45 +84,77 @@ function ProductionDetails() {
 
     return (
         <>
-            <h1>{selectedProduction.production_name}</h1>
+            
+            <h1 className="selectedProductionTitle">{selectedProduction.production_name}</h1>
 
-            <h3><u>Rehearsals</u></h3>
-            {productionRehearsals.map(rehearsal => {
-                return(
-                    <div key={rehearsal.id}>
-                        <Link to="/rehearsal/details" onClick={()=>onSelect(rehearsal)}>
-                            <h5>
-                                    {moment(rehearsal.start_time).format('MM-DD-YY')}
-                            </h5>
-                        </Link>
-                        <h5>{moment(rehearsal.start_time).format('h:mm a')} - {moment(rehearsal.end_time).format('h:mm a')}</h5>
-                    </div>
-                )
-            })}
+            <Box className="calendarForm">
+                <Box className="date1">
+                    <label htmlFor="startTime">Start time:</label>
+                    <input
+                        type="datetime-local"
+                        id="startTime"
+                        name="startTime"
+                        min="2022-02-08T18:00"
+                        max="2022-03-25T23:59"    
+                        onChange={event => setStartTime(event.target.value)}
+                    />
 
-            <label htmlFor="startTime">Start time:</label>
-            <input
-                type="datetime-local"
-                id="startTime"
-                name="startTime"
-                min="2022-02-08T18:00"
-                max="2022-03-25T23:59"    
-                onChange={event => setStartTime(event.target.value)}
-            />
+                </Box>
+                
+                <Box className="date2">
+                <label htmlFor="endTime">End time:</label>
+                <input
+                    type="datetime-local"
+                    id="endTime"
+                    name="endTime"
+                    min="2022-02-08T18:00"
+                    max="2022-03-25T23:59"  
+                    onChange={event => setEndTime(event.target.value)}  
+                />
 
-            <label htmlFor="endTime">End time:</label>
-            <input
-                type="datetime-local"
-                id="endTime"
-                name="endTime"
-                min="2022-02-08T18:00"
-                max="2022-03-25T23:59"  
-                onChange={event => setEndTime(event.target.value)}  
-            />
+                <Button 
+                    onClick={handleSubmit}
+                    variant="contained"
+                    sx={{
+                        background: "#191970",
+                        fontFamily: "Josefin Slab",
+                        marginLeft: "20px"
+                      }}
+                >
+                    Submit
+                </Button>
+                </Box>
+            </Box>
 
-            <button onClick={handleSubmit}>Submit</button>
+            <Box className="backBtn">
+                <Button 
+                    onClick={onBack}
+                    variant="contained"
+                    sx={{
+                      background: "#191970",
+                      fontFamily: "Josefin Slab"
+                    }}
+                >
+                    Back
+                </Button>
+            </Box>
 
-            <div><button onClick={onBack}>Back</button></div>
+            <h3 className="rehearsalTitle">Rehearsals</h3>
+
+            <Box className="rehearsalList" sx={{ width: "1200px"}}>
+                {productionRehearsals.map(rehearsal => {
+                    return(
+                        <div key={rehearsal.id} className="rehearsalTimes">
+                            <Link to="/rehearsal/details" onClick={()=>onSelect(rehearsal)}>
+                                <h5>
+                                        {moment(rehearsal.start_time).format('MM-DD-YY')}
+                                </h5>
+                            </Link>
+                            <h5>{moment(rehearsal.start_time).format('h:mm a')} - {moment(rehearsal.end_time).format('h:mm a')}</h5>
+                        </div>
+                    )
+                })}
+            </Box>
         
         </>
     )
